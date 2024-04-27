7ab594bb18a65c76d88fd59f3d99a0d4732d21d0
suwaloff <suwaloff@gmail.com>
2024-04-27 20:55:42 +0300

    Add documentation.

--- src/carousel/ui/utils/classNames.ts
+++ src/carousel/ui/utils/classNames.ts
@@ -1,4 +1,17 @@
+/**
+ * Type for class modifiers. Each key is a class name to be included if its value is true.
+ */
+
 export type Mods = Record<string, boolean | string | undefined>;
+
+/**
+ * Generates a string of class names for a component.
+ *
+ * @param {string} cls - Base class name.
+ * @param {Mods} mods - Classes to add if their value is true.
+ * @param {Array<string | undefined>} additional - Additional class names.
+ * @returns {string} A string of class names.
+ */
 
 export function classNames(
   cls: string,
